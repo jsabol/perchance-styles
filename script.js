7af71f62c05502c46f8761f5903a696c63bef8e1
Juliet Sabol <jsabol@theatlantic.com>
2022-03-15 14:40:02 -0400

    Updates

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 document
   .getElementById("output-container")
   .classList.add(location.search.includes("no-title") ? "no-title" : "");
+
+(() => {
+  let showLocks = true;
+
+  document.getElementById("hideLocks").addEventListener("click", () => {
+    [...document.getElementsByTagName("span")]
+      .filter((el) => el.innerText === "🔓︎" || el.innerText === "🔐︎")
+      .forEach((el) => (el.style.display = showLocks ? "none" : ""));
+    showLocks = !showLocks;
+  });
+})();
